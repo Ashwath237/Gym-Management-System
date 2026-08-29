3b7ae8d834f56f0133d37ceb8f90789f388e99f5
Ashwath <ashwathnair237@gmail.com>
2026-08-29 13:02:18 +0530

    Update database connection to use DATABASE_URL from Railway

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+connectionString: process.env.DATABASE_URL
 });
 
     app.use(express.json());
